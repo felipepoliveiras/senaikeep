f231021b0c62aff2ac3f0509e987f4634f35a6c9
Felipe Oliveira <felipeperoliveira@outlook.com>
2018-01-15 17:21:28 -0200

    Creating the webapp module

--- src/io/felipepoliveira/senaikeep/config/WebConfig.java
+++ src/io/felipepoliveira/senaikeep/config/WebConfig.java
@@ -1,10 +1,15 @@
 package io.felipepoliveira.senaikeep.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.ResourceBundleViewResolver;
 
 import io.felipepoliveira.senaikeep.ws.rest.v1.config.RestWebConfig;
 
@@ -14,5 +19,21 @@ import io.felipepoliveira.senaikeep.ws.rest.v1.config.RestWebConfig;
 @Import(value = {RestWebConfig.class})
 public class WebConfig extends WebMvcConfigurationSupport{
 	
+	@Bean
+	public ViewResolver getInternalResourceViewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		
+		return viewResolver;
+	}
+	
+	@Bean
+	public ViewResolver resourceBundleViewResolver() {
+	    ResourceBundleViewResolver bean = new ResourceBundleViewResolver();
+	    bean.setBasename("views");
+	    return bean;
+	}
 	
 }
